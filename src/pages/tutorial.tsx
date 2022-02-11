c7504618fb722b9bf84935eb6065a6314ec4d9c5
taurusboong2 <taurusboong2@gmail.com>
2022-02-11 16:19:37 +0900

    :global error, :local module working

--- src/pages/tutorial.tsx
+++ src/pages/tutorial.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from '../components/Button/index';
 import { useNavigate } from 'react-router-dom';
+import '../tutorial.module.scss';
 
 const Tutorial = () => {
   const navigate = useNavigate();
