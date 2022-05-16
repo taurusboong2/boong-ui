febbe1cf6ec8cc322bfaea884ca0ee64478ddf8c
ashnamuh <ashnamuh@gmail.com>
2022-05-16 19:28:14 +0900

    range 함수 추가

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,2 +1,14 @@
 // 1000에 1초. 밀리세컨드
 export const sleep = (delay: number) => new Promise(resolve => setTimeout(resolve, delay));
+
+export const range = (start: number, end: number, step = 1) => {
+  const last = end || start;
+  let i = last !== start ? start : 0;
+  const arr: number[] = [];
+
+  while (i < last) {
+    arr.push(i);
+    i += step;
+  }
+  return arr;
+};
